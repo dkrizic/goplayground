4b1b76a0d88230141461bb4dda6a5d1844201c73
Darko Krizic <darko@krizic.net>
2020-05-17 02:11:24 +0200

    Now using some other sample

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,11 @@
-FROM golang
+FROM golang:alpine AS build-env
 MAINTAINER darko@krizic.net
+RUN mkdir /go/src/app && apk update && apk add git
+ADD main.go /go/src/app/
+WORKDIR /go/src/app
+RUN CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -ldflags '-extldflags "-static"' -o app .
+
+FROM scratch
+WORKDIR /app
+COPY --from=build-env /go/src/app/app .
+ENTRYPOINT [ "./app" ]
--- main.go
+++ main.go
@@ -1,22 +1,20 @@
 package main
 
 import (
-	"log"
-	"net/http"
+   "log"
+   "net/http"
 )
 
-type server struct{}
+type Server struct{}
 
-func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(`{"message":"Hello, World"}`))
+func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+   w.WriteHeader(http.StatusOK)
+   w.Header().Set("Content-Type", "application/json")
+   w.Write([]byte(`{"message": "hello world"}`))
 }
 
 func main() {
-	s := &server{}
-	http.Handle("/", s)
-	log.Fatal(http.ListenAndServe(":8080",nil))
+   s := &Server{}
+   http.Handle("/", s)
+   log.Fatal(http.ListenAndServe(":8080", nil))
 }
-
-
